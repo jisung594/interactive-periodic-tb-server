4216dda80efd44d39b00c89f1344ff24d3530710
Jonathan Choi <jisung594@gmail.com>
2019-09-20 21:34:49 -0400

    heroku backend bug

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ app.use(bodyParser.json());
 
 // ***** mongod ******
 // PORT NUMBER: 27017
-app.db = mongoose.connect(process.env.MONGODB_URI)
+// mongoose.connect(process.env.MONGODB_URI  || 'mongodb://localhost:27017/periodic-table')
+mongoose.connect(process.env.MONGOLAB_URI  || 'mongodb://localhost:27017/periodic-table')
 
 // let db = mongoose.connection
 
