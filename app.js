ad5623fb96b369aee2f96036ab847ddc7d4dc333
Jonathan Choi <jisung594@gmail.com>
2019-09-21 20:53:15 -0400

    seeder?

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ var app = express();
 
 // ***** mongod ******
 // PORT NUMBER: 27017
-// mongoose.connect(process.env.MONGODB_URI  || 'mongodb://localhost:27017/periodic-table')
-mongoose.connect(process.env.MONGOLAB_URI  || 'mongodb://localhost:27017/periodic-table')
+mongoose.connect(process.env.MONGODB_URI  || 'mongodb://localhost:27017/periodic-table')
+// mongoose.connect(process.env.MONGOLAB_URI  || 'mongodb://localhost:27017/periodic-table')
 
 let db = mongoose.connection
 
