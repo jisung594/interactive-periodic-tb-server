00bffde547e6d32d0f0bfea9d04756cbe0e32557
Jonathan Choi <jisung594@gmail.com>
2019-09-21 21:12:57 -0400

    seeder?

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
+app.use('/', routes);
 // app.use('/users', usersRouter);
 app.use('/api/elements', require('./routes/api/elements'));
 
