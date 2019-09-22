ceb1f0aaf28b30851d821e918706861073a27fc9
Jonathan Choi <jisung594@gmail.com>
2019-09-21 21:39:12 -0400

    routes?

--- routes/api/elements.js
+++ routes/api/elements.js
@@ -3,7 +3,7 @@ const router = express.Router()
 let Element = require('../../models/element.js')
 
 router.get('/', (req,res) => {
-  console.log("I received a GET request");
+  console.log("I received a GET request ##");
 
   Element.find()
     // .then(data => res.json(data))
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log("I received a GET request");
-  
+router.get('/', function(req, res, next) {  
   res.render('index', { title: 'Express' });
 });
 
