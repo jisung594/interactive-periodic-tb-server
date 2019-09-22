cfe8ede9a58dbbe6916d4b3b7ee96f9c842d06e1
Jonathan Choi <jisung594@gmail.com>
2019-09-22 03:15:59 -0400

    connected to mlab database

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 
 let db = mongoose.connection
 
+// let Element = require('../../models/element.js')
+// Element.find()
+
+app.get('/api/elements', (req,res) => {
+  res.send(db.elements)
+})
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -34,11 +41,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api/elements', require('./routes/api/elements'));
-
-app.get('/api/elements', (req,res) => {
-  let elementRoutes = require('./routes/api/elements')
-  res.send(elementRoutes)
-});
 
 
 // catch 404 and forward to error handler
