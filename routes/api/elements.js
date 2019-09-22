b401a909b8525cfbb19ad170954b87490f51b484
Jonathan Choi <jisung594@gmail.com>
2019-09-22 01:10:33 -0400

    routes?

--- routes/api/elements.js
+++ routes/api/elements.js
@@ -5,20 +5,35 @@ let Element = require('../../models/element.js')
 var mongoose = require('mongoose')
 
 
-router.get('/', (req,res) => {
-  console.log("I received a GET request");
+// router.get('/', (req,res) => {
+//   console.log("I received a GET request");
+//
+//   Element.find()
+//     .then(data => res.json(data))
+//     .catch(error)
+//
+//   // mongoose.connect(process.env.MONGODB_URI)
+//   //
+//   // let db = mongoose.connection
+//   // db.elements.find()
+//   //   .then(data => res.json(data))
+//
+// })
 
-  Element.find(req.query)
-    .then(data => res.json(data))
-    .catch(error)
 
-  // mongoose.connect(process.env.MONGODB_URI)
-  //
-  // let db = mongoose.connection
-  // db.elements.find()
-  //   .then(data => res.json(data))
+router.get('/', async (req, res) => {
+    try {
+        const elements = await Element.find({});
 
-})
+        return res.json({
+            elements
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Internal Server error'
+        });
+    }     
+});
 
 
 module.exports = router;
