f6f14509bf6722a4d496926869448bb1dedc5a9e
Jonathan Choi <jisung594@gmail.com>
2019-09-22 01:42:46 -0400

    routes?

--- app.js
+++ app.js
@@ -52,15 +52,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-//
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
-
-
 // PORT
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Listening on port ${port}`))
--- seeds/element-seeder.js
+++ seeds/element-seeder.js
@@ -10,28 +10,25 @@ db.collection('elements').remove({})
 let elementsData = PeriodicTable
 let counter = 1
 
-// let elements
-// if (elementsData.length) {
-  let elements = elementsData["elements"].map(ele => {
-    return new Element({
-      id: ele[counter],
-      name: ele["name"],
-      symbol: ele["symbol"],
-      appearance: ele["appearance"],
-      phase: ele["phase"],
-      number: ele["number"],
-      period: ele["period"],
-      density: ele["density"],
-      summary: ele["summary"],
-      spectral_img: ele["spectral_img"],
-      category: ele["category"],
-      discovered_by: ele["discovered_by"],
-      named_by: ele["named_by"]
-    })
-
-    counter++
+let elements = elementsData["elements"].map(ele => {
+  return new Element({
+    id: ele[counter],
+    name: ele["name"],
+    symbol: ele["symbol"],
+    appearance: ele["appearance"],
+    phase: ele["phase"],
+    number: ele["number"],
+    period: ele["period"],
+    density: ele["density"],
+    summary: ele["summary"],
+    spectral_img: ele["spectral_img"],
+    category: ele["category"],
+    discovered_by: ele["discovered_by"],
+    named_by: ele["named_by"]
   })
-// }
+
+  counter++
+})
 
 
 let done = 0;
