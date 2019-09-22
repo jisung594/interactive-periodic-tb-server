7a291b7ecfd17e0d3dbe7823b25f42594d2a4635
Jonathan Choi <jisung594@gmail.com>
2019-09-22 01:25:57 -0400

    routes?

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
 let elementsData = require('./PeriodicTableJSON.json')
-
+require('dotenv').config();
 
 
 var routes = require('./routes/index');
@@ -52,6 +52,14 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 // PORT
