0479eb76821a1f6fc7594213afc10cb4cb6c2938
Jonathan Choi <jisung594@gmail.com>
2019-09-23 17:00:45 -0400

    declaring routes

--- app.js
+++ app.js
@@ -49,12 +49,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-})
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', "*");
+//   res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// })
 
 
 // catch 404 and forward to error handler
@@ -72,6 +72,18 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+const dev = app.get('env') !== 'production';
+
+if(!dev){
+  app.disable('x-powered-by');
+  app.use(express.static(path.resolve(__dirname, 'client/build')));
+  app.get('*',(req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
+  })
+};
+
 
 // PORT
 const port = process.env.PORT || 5000
